bc12066fb1373a7a4d32f27d90777ff8e4df565a
Sena <hongchew96@gmail.com>
2023-06-24 17:24:47 +0900

    added header link to home page

--- src/components/FilmDetails.jsx
+++ src/components/FilmDetails.jsx
@@ -6,13 +6,13 @@ const FilmDetails = ({ film, people }) => {
 	return (
 		<>
 			<div className='m-10 flex flex-row justify-center border-2 rounded-3xl'>
-				<img className='w-1/2 max-h-screen rounded-3xl basis-1/3' src={film.image}></img>
+				<img className='max-h-screen rounded-3xl basis-1/3 w-full h-auto' src={film.image}></img>
 				<div className='basis-2/3'>
 					<div className='p-8'>
 						<h1 className='text-5xl'>
 							{film.title}
 						</h1>
-                        <p className='m-2'>{film.original_title} &#40;{film.original_title_romanised}&#41;</p>
+                        <p className='m-2'>{film.original_title} &#40;<span className='italic'>{film.original_title_romanised}</span>&#41;</p>
 					</div>
 					<div className='px-8 py-2'>
 						<p>
